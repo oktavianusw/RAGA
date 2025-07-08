d6963feeea168ddc9c4b77a74fdab72da431fbba
Aditbalboa <adit97r@gmail.com>
2025-07-08 22:22:05 +0800

    Initial Commit

---
+++ RAGA/RAGAApp.swift
@@ -0,0 +1,17 @@
+//
+//  RAGAApp.swift
+//  RAGA
+//
+//  Created by Aditbalboa on 7/8/25.
+//
+
+import SwiftUI
+
+@main
+struct RAGAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
